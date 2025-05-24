a294e0eb14a3366357488f6dbe804da4eca195a6
Anton <122563428+anton3029941@users.noreply.github.com>
2025-05-24 13:09:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+import subprocess
+import tkinter as tk
+import threading
+
+
+# Start threading to avoid blocking the GUI
+def start_download():
+    url = entry.get()
+    mode = var.get()
+
+    thread = threading.Thread(target=dl, args=(url, mode, label), daemon=True)
+    thread.start()
+
+# Function to handle the download process
+def dl(url, mode, label):
+    label.config(text="Starting download...")
+
+    # Choosing the command based on the mode
+    if mode == "audio":
+        command = [
+            "yt-dlp",
+            "--format", "bestaudio/best",
+            "--extract-audio",
+            "--audio-format", "mp3",
+            "--audio-quality", "320K",
+            "--cookies", "cookies.txt",
+            "-o", "downloads/%(title)s.%(ext)s",
+            url
+        ]
+    else:
+        command = [
+            "yt-dlp",
+            "--format", "bestvideo+bestaudio/best",
+            "--cookies", "cookies.txt",
+            "-o", "downloads/%(title)s.%(ext)s",
+            url
+        ]
+
+    # Output success or error message
+    try:
+        subprocess.run(command, check=True)
+        label.config(text="Download finished.")
+    except subprocess.CalledProcessError as err:
+        label.config(text=f"Ошибка: {err}")
+
+# GUI
+root = tk.Tk()
+root.title("YouTube Downloader 2000")
+root.geometry("600x350")
+
+entry = tk.Entry(root, width=50)
+entry.pack(pady=10)
+entry.focus_set()
+
+var = tk.StringVar(value="audio")
+
+frame = tk.Frame(root)
+frame.pack()
+
+radio_audio = tk.Radiobutton(frame, text="Audio (MP3)", variable=var, value="audio")
+radio_audio.pack(side="left", padx=10)
+
+radio_video = tk.Radiobutton(frame, text="Video (MP4)", variable=var, value="video")
+radio_video.pack(side="left", padx=10)
+
+button = tk.Button(root, text="Download", width=20, height=2, command=start_download)
+button.pack(pady=10)
+
+label = tk.Label(root, text="Enter a YouTube video/playlist URL")
+label.pack(pady=10)
+
+root.mainloop()
